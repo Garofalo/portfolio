db01d84e4a9e1ebdbeeea2a2e2e13de49feee9a9
Owner <garofalo.ronald@gmail.com>
2022-02-14 16:27:21 -0500

    added show more

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,21 +1,21 @@
 import './Footer.css'
 import git from '../../utils/github.svg'
+import { useNavigate } from 'react-router-dom'
 
 export default function Footer(){
+    const nav = useNavigate()
     return (
     <footer id="contact">
             <h1 style={{
             borderBottom: '2px solid black',
              marginTop: '5vh', 
-             flexBasis: '92vw'}}>Sources</h1>
+             flexBasis: '92vw'}}>Contact</h1>
              <div className='contact-info'>
-                <div className='a-contact'>
-                    <img className='white-bg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" />
+             <div className='a-contact'>
+                    <a href='https://github.com/Garofalo?tab=repositories' target='_blank'><button >Public Github Repositories</button></a>
                 </div>
                 <div className='a-contact'>
-                    <img src={git}/>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-
+                    <a href='https://www.linkedin.com/in/ron-garo/' target='_blank'><button >Message me on Linked In</button></a>
                 </div>
              </div>
     </footer>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,8 +5,16 @@ import mam from '../../utils/mam.png'
 import randq from '../../utils/randq.png'
 import restLogo from '../../utils/rest.png'
 import Footer from '../Footer/Footer';
+import { useState } from 'react';
 
 export default function Home(){
+    const [showMore, setShowMore] = useState({
+        proj1 : false,
+        proj2 : false,
+        proj3: false
+    })
+    
+
 
     return (
     <div>
@@ -61,42 +69,124 @@ export default function Home(){
         <div className="projects container-home" >
             <h1 style={{borderBottom: '2px solid black', marginTop: '5vh'}}>Projects</h1>
             <div className='proj-card-holder'>
-            <Card style={{width: '20rem'}}>
+            <Card id="proj1"  >
                 <Card.Img  variant= "top" src={mam} style={{width: '30vw' , margin: '0 auto'}}/>
-                <Card.Body id="proj1">
+                <Card.Body >
                     <Card.Title >
                         Mamoo
                     </Card.Title>
                     <Card.Text >
                         A Full Stack Application that authenticated users can use to mark important events in their lives.
                     </Card.Text>
+
+                
+                {
+                    showMore.proj1 === true && <>
+                    <Card.Text>Deployed: Yes</Card.Text>
+                    <div className='proj-icons'>
+
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                        </div>
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+                        </div>
+                        <div className="icon-holder">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                        </div>
+                        <div className="icon-holder">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-original.svg" />
+                        </div>
+                        <div className="icon-holder">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+                        </div>
+                    </div>
+                    <div className='link-holder'>
+                    <Card.Link href="https://yourmamoo.netlify.app/" target="_blank">Deployed Site</Card.Link>
+                    <Card.Link href="https://github.com/Garofalo/mamoo" target="_blank">Front End Repository</Card.Link>
+                    <Card.Link href="https://github.com/Garofalo/mamoobackend" target="_blank">Back End Repository</Card.Link>
+                    </div>
+                    </>
+                }
                 </Card.Body>
+                <div>
+                <button className="show-more-button" onClick={()=>setShowMore({...showMore, proj1: !showMore.proj1})}>Show More</button>
+                </div>
             </Card>
             </div>
             <div className='proj-card-holder'>
-            <Card style={{width: '20rem'}}>
+            <Card id="proj2" >
                 <Card.Img variant= "top" src={randq} style={{width: '30vw', margin: '0 auto'}}/>
-                <Card.Body id="proj2">
+                <Card.Body >
                     <Card.Title>
                         Random Quest
                     </Card.Title>
                     <Card.Text>
                         A game built using React.js where users generate random enemies, attacks and outcomes.
                     </Card.Text>
+                    {
+                    showMore.proj2 === true && <>
+                    <Card.Text>Deployed: Yes</Card.Text>
+                    <div className='proj-icons'>
+
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                        </div>
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+                        </div>
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+                        </div>
+
+                    </div>
+                    <div className='link-holder'>
+                    <Card.Link href="https://randomquest.netlify.app/" target="_blank">Deployed Site</Card.Link>
+                    <Card.Link href="https://github.com/Garofalo/random-quest" target="_blank">GitHub Repository</Card.Link>
+                    </div>
+                    </>
+                }
                 </Card.Body>
+                <div>
+                <button className="show-more-button" onClick={()=>setShowMore({...showMore, proj2: !showMore.proj2})}>Show More</button>
+                </div>
             </Card>
             </div>
             <div className='proj-card-holder'>
-            <Card style={{width: '20rem'}}>
+            <Card id="proj3">
                 <Card.Img variant= "top" src={restLogo} style={{width: '30vw', margin: '0 auto'}}/>
-                <Card.Body id="proj3">
+                <Card.Body >
                     <Card.Title>
                         RESTful API
                     </Card.Title>
                     <Card.Text>
                         A demonstration of a self-built RESTful API and appropriate documentation.
                     </Card.Text>
+                    {
+                    showMore.proj3 === true && <>
+                    <Card.Text>Deployed: No</Card.Text>
+                    <div className='proj-icons'>
+
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                        </div>
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />
+                        </div>
+                        <div className="icon-holder">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+                        </div>
+
+                    </div>
+                    <div className='link-holder'>
+                        <Card.Link href="https://github.com/Garofalo/random-quest" target="_blank">GitHub Repository</Card.Link>
+                    </div>
+                    </>
+                }
                 </Card.Body>
+                <div>
+                <button className="show-more-button" onClick={()=>setShowMore({...showMore, proj3: !showMore.proj3})}>Show More</button>
+                </div>
             </Card>
             </div>
 
