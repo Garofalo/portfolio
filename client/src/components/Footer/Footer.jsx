a357af504222ad1a66e583b5bb65d0d90c442fca
Owner <garofalo.ronald@gmail.com>
2022-02-16 11:25:44 -0500

    resume

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,16 +1,11 @@
 import './Footer.css'
-// import { saveAs } from 'file-saver'
-// import resume from '../../utils/RonGarofalo.pdf'
+
+import resume from '../../utils/RonGarofalo.pdf'
 
 
 
 export default function Footer(){
-    // const saveFile = () => {
-    //     saveAs(
-    //         "http://www.ron-garofalo.netlify.app/src/utils/resume.pdf",
-    //         "RonGarofalo.pdf"
-    //         )
-    // }
+
     return (
     <footer id="contact">
             <h1 style={{
@@ -24,8 +19,9 @@ export default function Footer(){
                 <div className='a-contact'>
                     <a rel="noreferrer" href='https://www.linkedin.com/in/ron-garo/' target='_blank'>Message me on Linked In</a>
                 </div>
-                {/* <div className='a-contact'>
-                    <a href='../../utils/RonGarofalo.pdf' download="RonGarofaloResume">Download my resume</a>             </div> */}
+            <div className='a-contact'>
+                    <a href={resume} download="RonGarofaloResume.pdf">Download my resume</a>             
+            </div>
 
                 </div>
             
