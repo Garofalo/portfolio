e3d08d0af45a513447daaaaf7c14ac9b6feabb1f
Owner <garofalo.ronald@gmail.com>
2022-02-16 16:34:57 -0500

    google

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import Home from "./screens/Home/Home";
 import NavBar from "./components/Nav/Nav";
+import { useEffect } from "react";
+import ReactGA from "react-ga"
 function App() {
+   
+  useEffect(()=>{
+
+    ReactGA.initialize("UA-220602629-1")
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  })
+
+
+
+  
   return (
     <div className="App">
       <NavBar />
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -12,18 +12,18 @@ export default function Footer(){
             borderBottom: '2px solid black',
              marginTop: '5vh', 
              width: '92vw'}}>Contact</h1>
-             <div className='contact-info'>
+            <div className='contact-info'>
                 <div className='a-contact'>
                     <a rel="noreferrer" href='https://github.com/Garofalo?tab=repositories' target='_blank'>Public GitHub Repositories</a>
                 </div>
                 <div className='a-contact'>
                     <a rel="noreferrer" href='https://www.linkedin.com/in/ron-garo/' target='_blank'>Message me on Linked In</a>
                 </div>
-            <div className='a-contact'>
-                    <a href={resume} download="RonGarofaloResume.pdf">Download my resume</a>             
-            </div>
-
+                <div className='a-contact'>
+                        <a href={resume} download="RonGarofaloResume.pdf">Download my resume</a>             
                 </div>
+
+            </div>
             
     </footer>
     )
